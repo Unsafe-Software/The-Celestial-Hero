50fd1973a557b7390341e1032950f7434525176b
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2024-01-15 20:34:42 +0700

    Add namespace

--- src/entities/entity.cpp
+++ src/entities/entity.cpp
@@ -2,64 +2,63 @@
 #include <raylib.h>
 #include "../world/world.hpp"
 
-void Entity::resolveCollisions() {
-    //TODO
-}
+namespace Entities {
+    void Entity::resolveCollisions() {
+        //TODO
+    }
 
+    Entity::Entity(Rectangle Bound, Texture2D Texture, Map::World* World) {
+        bound = Bound;
+        texture = Texture;
+        world = World;
+    }
 
-Entity::Entity(Rectangle Bound, Texture2D Texture, Map::World* World) {
-    bound = Bound;
-    texture = Texture;
-    world = World;
-}
+    void Entity::Move(float x, float y) {
+        bound.x += x;
+        bound.y += y;
+        resolveCollisions();
+    }
 
+    Rectangle Entity::GetBound() {
+        return bound;
+    }
 
-void Entity::Move(float x, float y) {
-    bound.x += x;
-    bound.y += y;
-    resolveCollisions();
-}
+    Vector2 Entity::GetPos() {
+        return (Vector2){bound.x, bound.y};
+    }
 
+    Vector2 Entity::GetSize() {
+        return (Vector2){bound.width, bound.height};
+    }
 
-Rectangle Entity::GetBound() {
-    return bound;
-}
+    void Entity::SetBound(Rectangle Buond) {
+        bound = Buond;
+        resolveCollisions();
+    }
 
-Vector2 Entity::GetPos() {
-    return (Vector2){bound.x, bound.y};
-}
+    void Entity::SetPos(Vector2 Pos) {
+        bound.x = Pos.x;
+        bound.y = Pos.y;
+        resolveCollisions();
+    }
 
-Vector2 Entity::GetSize() {
-    return (Vector2){bound.width, bound.height};
-}
+    void Entity::SetSize(Vector2 Size) {
+        bound.width = Size.x;
+        bound.height = Size.y;
+        resolveCollisions();
+    }
 
-void Entity::SetBound(Rectangle Buond) {
-    bound = Buond;
-    resolveCollisions();
-}
+    void Entity::SetWorld(Map::World* NewWorld, float x, float y) {
+        world = NewWorld;
+        bound.x = x;
+        bound.y = y;
+        resolveCollisions();
+    }
 
-void Entity::SetPos(Vector2 Pos) {
-    bound.x = Pos.x;
-    bound.y = Pos.y;
-    resolveCollisions();
-}
-
-void Entity::SetSize(Vector2 Size) {
-    bound.width = Size.x;
-    bound.height = Size.y;
-    resolveCollisions();
-}
-
-void Entity::SetWorld(Map::World* NewWorld, float x, float y) {
-    world = NewWorld;
-    bound.x = x;
-    bound.y = y;
-    resolveCollisions();
-}
-
-void Entity::Draw() {
-    DrawTexturePro(
-        texture, (Rectangle){0.0f, 0.0f, texture.width, texture.height},
-        bound, (Vector2){0, 0}, 0.0f, WHITE
-    );
+    void Entity::Draw() {
+        DrawTexturePro(
+            texture, (Rectangle){0.0f, 0.0f, texture.width, texture.height},
+            bound, (Vector2){0, 0}, 0.0f, WHITE
+        );
+    }
 }
--- src/entities/entity.hpp
+++ src/entities/entity.hpp
@@ -2,25 +2,27 @@
 #include <raylib.h>
 #include "../world/world.hpp"
 
-class Entity {
-    Map::World* world;
-    Rectangle bound;
-    Texture2D texture;
+namespace Entities {
+    class Entity {
+        Map::World* world;
+        Rectangle bound;
+        Texture2D texture;
 
-    void resolveCollisions();
+        void resolveCollisions();
 
-public:
-    Entity(Rectangle Bound, Texture2D Texture, Map::World* World);
+    public:
+        Entity(Rectangle Bound, Texture2D Texture, Map::World* World);
 
-    void Move(float x, float y);
-    
-    Rectangle GetBound();
-    Vector2 GetPos();
-    Vector2 GetSize();
-    void SetBound(Rectangle Buond);
-    void SetPos(Vector2 Pos);
-    void SetSize(Vector2 Size);
-    void SetWorld(Map::World* NewWorld, float x, float y);
+        void Move(float x, float y);
+        
+        Rectangle GetBound();
+        Vector2 GetPos();
+        Vector2 GetSize();
+        void SetBound(Rectangle Buond);
+        void SetPos(Vector2 Pos);
+        void SetSize(Vector2 Size);
+        void SetWorld(Map::World* NewWorld, float x, float y);
 
-    void Draw();
-};
+        void Draw();
+    };
+}
